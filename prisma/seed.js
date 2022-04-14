d39e17e54239299980e969065e05aeb1444e93f0
Samir Shrestha <theonlysamir@gmail.com>
2022-04-14 16:14:12 +0545

    First Commit

---
+++ prisma/seed.js
@@ -0,0 +1,39 @@
+const { PrismaClient } = require('@prisma/client')
+const { faker } = require('@faker-js/faker');
+const prisma = new PrismaClient()
+const bcrypt = require('bcrypt')
+
+async function main() {
+  const admin = await prisma.user.upsert({
+    where: { email: 'admin@admin.com' },
+    update: {},
+    create: {
+      email: 'admin@admin.com',
+      name: 'Admin',
+      role: "ADMIN",
+      password: await bcrypt.hash('password',10)
+    },
+  })
+
+  const visitor = await prisma.user.upsert({
+    where: { email: 'visitor@visitor.com' },
+    update: {},
+    create: {
+      email: 'visitor@visitor.com',
+      name: 'Visitor',
+      role:"VISITOR",
+      password: await bcrypt.hash('password',10)
+    },
+  })
+
+  console.log({ admin,visitor,guide })
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
